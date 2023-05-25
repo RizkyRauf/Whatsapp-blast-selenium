b6551ca2f2993ce0590be8e71b1bc3d45160918f
Ahcmad Riski Rauf Aridho <95733130+RizkyRauf@users.noreply.github.com>
2023-05-25 22:24:42 +0700

    Update wa.py

--- wa.py
+++ wa.py
@@ -1,6 +1,4 @@
 import time
-import mysql.connector
-from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,77 +6,96 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
-#Bs
 import pandas as pd
 
-#regex
-import re
+# Fungsi untuk mengirim pesan dan menghitung jumlah pesan terkirim
+def kirim_pesan(number, pesan):
+    try:
+        # Tentukan jalur ke ChromeDriver Anda
+        chrome_driver_path = '[path driver chrome ]'
 
-#append
-pesan = []
-number_phone = []
+        # Inisialisasi Service object
+        service = Service(chrome_driver_path)
 
-#phone target yang akan dikirim
-read_phone = pd.read_excel('test.xlsx')
+        # Inisialisasi Options object
+        options = Options()
+        options.add_argument("user-data-dir=[path user]")
+
+        # Inisialisasi WebDriver dengan menggunakan Service object dan Options object
+        driver = webdriver.Chrome(service=service, options=options)
+
+        url = 'https://api.whatsapp.com/send/?phone=62{}&text&type=phone_number&app_absent=0'.format(number)
+        driver.get(url)
+        time.sleep(2)
+
+        # Klik lanjut chat
+        wait = WebDriverWait(driver, 100)
+        lanjut_path = "//a[contains(@title,\"Bagikan di WhatsApp\")]"
+        lanjut = wait.until(EC.presence_of_element_located((By.XPATH, lanjut_path)))
+        lanjut.click()
+        time.sleep(2)
+
+        # Gunakan WhatsApp Web
+        gunakan_path = "//h4[@class='_9vd5'][2]/a[@class='_9vcv _9vcx']/span[@class='_advp _aeam']"
+        gunakan = wait.until(EC.presence_of_element_located((By.XPATH, gunakan_path)))
+        time.sleep(2)
+        gunakan.click()
+
+        # Masukkan pesan per paragraf
+        message_box_path = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
+        message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_path)))
+
+        for message in pesan:
+            message_box.send_keys(message)
+            message_box.send_keys(Keys.SHIFT, '\n')
+            message_box.send_keys(Keys.SHIFT, '\n')
+
+        message_box.send_keys(Keys.ENTER)
+        print ("=============================================")
+        print("Berhasil mengirim pesan kepada {}".format(number))
+        print ("=============================================")
+        time.sleep(5)
+
+        # Cek tanda centang ganda untuk memastikan pesan terkirim
+        sent_tick_path = "//span[@data-testid='msg-dblcheck']"
+        sent_tick = driver.find_elements(By.XPATH, sent_tick_path)
+        pesan_terkirim = bool(sent_tick)
+
+        driver.quit()
+
+        return pesan_terkirim
+
+    except Exception:
+        print ("=============================================")
+        print("Terjadi kesalahan saat mengirim pesan kepada {}".format(number))
+        print ("=============================================")
+        return False
+
+
+# Baca data phone.xlsx
+read_phone = pd.read_excel('phone.xlsx')
+
+# Baca pesan dari file pesan.txt
+with open('pesan.txt', 'r', encoding="utf8") as file:
+    pesan_tergabung = file.read()
+
+pesan = pesan_tergabung.split('\n')
+
+# Inisialisasi variabel jumlah pesan terkirim dan jumlah tidak memiliki WhatsApp Web
+pesan_terkirim = 0
+tidak_punya_whatsapp_web = 0
+
+# Kirim pesan untuk setiap nomor telepon
 for i in range(len(read_phone)):
-    number = read_phone['phone'][i]  # number berisi lebih dari 1 value
+    number = str(read_phone['phone'][i])  # number berisi lebih dari 1 value
 
-    # pesan yang akan dikirim
-    with open('pesan.txt', 'r', encoding="utf8") as file:
-        pesan_tergabung = file.read()
+    # Kirim pesan dan periksa apakah terkirim
+    terkirim = kirim_pesan(number, pesan)
 
-    pesan = pesan_tergabung.split('\n')
-
-    # Tentukan jalur ke ChromeDriver Anda
-    chrome_driver_path = ''
-
-    # Inisialisasi Service object
-    service = Service(chrome_driver_path)
-
-    # Inisialisasi Options object
-    options = Options()
-    options.add_argument("")  # Contoh penggunaan opsi lainnya
-
-    # Inisialisasi WebDriver dengan menggunakan Service object dan Options object
-    driver = webdriver.Chrome(service=service, options=options)
-
-    url = 'https://api.whatsapp.com/send/?phone=62{}&text&type=phone_number&app_absent=0'.format(number)
-    driver.get(url)
-
-    # klik lanjut chat
-    wait = WebDriverWait(driver, 100)
-    lanjut_path = "//a[contains(@title,\"Bagikan di WhatsApp\")]"
-    lanjut = wait.until(EC.presence_of_element_located((By.XPATH, lanjut_path)))
-    lanjut.click()
-    print("berhasil klik lanjut chat kepada {}".format(number))
-    time.sleep(4)
-
-    # gunakan Whatsapp Web
-    gunakan_path = "//h4[@class='_9vd5'][2]/a[@class='_9vcv _9vcx']/span[@class='_advp _aeam']"
-    gunakan = wait.until(EC.presence_of_element_located((By.XPATH, gunakan_path)))
-    time.sleep(4)
-    gunakan.click()
-    print("berhasil gunakan whatsapp kepada {}".format(number))
-
-    # Masukkan pesan per paragraf
-    message_box_path = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
-    message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_path)))
-
-    for message in pesan:
-        message_box.send_keys(message)
-        message_box.send_keys(Keys.SHIFT, '\n')
-        message_box.send_keys(Keys.SHIFT, '\n')
-
-    message_box.send_keys(Keys.ENTER)
-    time.sleep(10)
-
-    # Cek tanda centang ganda untuk memastikan pesan terkirim
-    sent_tick_path = "//span[@data-testid='msg-dblcheck']"
-    sent_tick = driver.find_elements(By.XPATH, sent_tick_path)
-    if sent_tick:
-        print("Pesan terkirim")
+    if terkirim:
+        pesan_terkirim += 1
     else:
-        print("Pesan tidak terkirim")
+        tidak_punya_whatsapp_web += 1
 
-    driver.quit()
+print("Jumlah pesan terkirim: {}".format(pesan_terkirim))
+print("Jumlah tidak memiliki WhatsApp Web: {}".format(tidak_punya_whatsapp_web))
